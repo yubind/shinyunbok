9cb39a8541e4f8dbb305f2f3e9339085e83c19d3
Yubin Kim <yubind91@gmail.com>
2018-11-20 21:28:17 +0000

    add menu btn animation

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,27 @@
-const menuBtn = $('.menu-btn');
-const menu = $('.menu');
-const menuBrand = $('.menu-brand');
-const menuItems = $('.menu-items');
-const menuItem = $('.menu-item');
-
-menuBtn.on('click', function() {
-    menu.toggleClass('visible');
+$(document).ready(function() {
+    $('body').css({ "display" : "block" });
+    
+    //set initial state of menu
+    let showMenu = false;
+    $('.menu-btn').click(toggleMenu);
+    
+    function toggleMenu() {
+        if(!showMenu) {
+            $('.menu-btn').addClass('close');
+            $('.menu-items').addClass('show');
+            $('.menu-item').addClass('show');
+            $('.menu-brand').addClass('show');
+            
+            showMenu = true;
+             
+        } else {
+            $('.menu-btn').removeClass('close');
+            $('.menu-items').removeClass('show');
+            $('.menu-item').removeClass('show');
+            $('.menu-brand').removeClass('show');
+            
+            showMenu = false;
+        }
+    }
 });
+
